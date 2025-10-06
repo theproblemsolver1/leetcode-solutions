1b1af591c308d8a9300689b804eca9e5c68e83d5
theproblemsolver1 <tdamage134@gmail.com>
2025-10-06 23:42:47 +0530

    .sql Easy

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+SELECT customer_id, COUNT(customer_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions
+ON Transactions.visit_id = Visits.visit_id
+WHERE transaction_id IS NULL
+GROUP BY customer_id
+ORDER BY customer_id;
