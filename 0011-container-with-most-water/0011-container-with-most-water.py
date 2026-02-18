f6090624d7ae47cdafa92121ef5da7e5983130ed
theproblemsolver1 <tdamage134@gmail.com>
2026-02-18 19:32:24 +0530

    .py Medium

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0  # initialize max_area
+
+        while left < right:
+            Height = min(height[left], height[right])
+            width = right - left
+            area = Height * width
+            max_area = max(max_area, area)
+
+            # Move the shorter pointer
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+        
