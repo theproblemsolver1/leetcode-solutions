2f488d3e51b3bde48d35ff9cb3864cb8b2c5da4f
theproblemsolver1 <tdamage134@gmail.com>
2026-04-06 21:02:19 +0530

    .py Easy

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,30 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        
+        if not p or not q:
+            return False
+        
+        if p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and 
+                self.isSameTree(p.right, q.right))
+
+
+p = TreeNode(1, TreeNode(2), TreeNode(3))
+q = TreeNode(1, TreeNode(2), TreeNode(3))
+
+obj = Solution()
+result = obj.isSameTree(p, q)
+
+print(result)
