be69eac4ea63a5be983b4862f93b8250205f6a6f
theproblemsolver1 <tdamage134@gmail.com>
2026-04-09 21:48:00 +0530

    .py Easy

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row=[1]
+        for i in range(1,rowIndex+1):
+            new_row=[1]
+            for j in range(1,i):
+                new_row.append(row[j-1] + row[j])
+            new_row.append(1)
+            row=new_row
+        return row
+        
